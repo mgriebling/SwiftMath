9bcf6e8c704b3720f3d10d36609841eb21c085bb
Michael Griebling <sam@c-inspirations.com>
2023-01-16 16:42:27 -0500

    Fixed a colour conversion issue.

--- Sources/SwiftMathRender/MathRender/MTColor.swift
+++ Sources/SwiftMathRender/MathRender/MTColor.swift
@@ -17,8 +17,8 @@ extension MTColor {
         let scanner = Scanner(string: hexString)
         scanner.charactersToBeSkipped = CharacterSet(charactersIn: "#")
         scanner.scanHexInt64(&rgbValue)
-        return MTColor(red: CGFloat((rgbValue & 0xFF0000))/255.0,
-                       green: CGFloat((rgbValue & 0xFF00))/255.0,
+        return MTColor(red: CGFloat((rgbValue & 0xFF0000) >> 16)/255.0,
+                       green: CGFloat((rgbValue & 0xFF00) >> 8)/255.0,
                        blue: CGFloat((rgbValue & 0xFF))/255.0, alpha: 1.0)
     }
     
