695c8a6b84d343c4fd05400c5894a15ff12a55e4
Michael Griebling <sam@c-inspirations.com>
2023-06-17 10:04:36 -0400

    Remove CoreGraphics dependency.

--- Package.swift
+++ Package.swift
@@ -2,7 +2,6 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-import CoreGraphics
 
 let package = Package(
     name: "SwiftMath",
