d6493a7d6110ed764f26e4ea9857ae7cfdb63eb3
Sven A. Schmidt <sas@finestructure.co>
2023-06-17 15:31:12 +0200

    Update Package.swift
    Allow `package-dump` on Linux

--- Package.swift
+++ Package.swift
@@ -2,7 +2,9 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+#if !os(Linux)
 import CoreGraphics
+#endif
 
 let package = Package(
     name: "SwiftMath",
