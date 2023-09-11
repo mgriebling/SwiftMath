32ba7c128ade8d309a104aabd346f8379f31d656
Peter Tang <petersktang@gmail.com>
2023-09-12 07:18:44 +0800

    added MTMathImage - draw latex image offscreen.

--- Sources/SwiftMath/MathRender/MTConfig.swift
+++ Sources/SwiftMath/MathRender/MTConfig.swift
@@ -18,6 +18,7 @@ public typealias MTBezierPath = UIBezierPath
 public typealias MTLabel = UILabel
 public typealias MTEdgeInsets = UIEdgeInsets
 public typealias MTRect = CGRect
+public typealias MTImage = UIImage
 
 let MTEdgeInsetsZero = UIEdgeInsets.zero
 func MTGraphicsGetCurrentContext() -> CGContext? { UIGraphicsGetCurrentContext() }
@@ -31,6 +32,7 @@ public typealias MTColor = NSColor
 public typealias MTBezierPath = NSBezierPath
 public typealias MTEdgeInsets = NSEdgeInsets
 public typealias MTRect = NSRect
+public typealias MTImage = NSImage
 
 let MTEdgeInsetsZero = NSEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 0)
 func MTGraphicsGetCurrentContext() -> CGContext? { NSGraphicsContext.current?.cgContext }
